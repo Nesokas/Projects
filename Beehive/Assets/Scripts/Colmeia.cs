3e184d12fb9ceba4dac0275bee2f8b64ced07971
Nuno Costa <nuno.pinto.costa@gmail.com>
2015-02-28 02:42:05 +0000

    Started Beehive.

---
+++ Beehive/Assets/Scripts/Colmeia.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using SQLite4Unity3d;
+
+public class Colmeia {
+
+	[PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
+	private string name;
+
+	private struct Date {
+		public int day;
+		public int month;
+		public int year;
+	}
+
+	private enum Actions {
+	
+		ACCAO_A,
+		ACCAO_B,
+		ACCAO_C,
+		ACCAO_D
+		
+	}
+
+	private Actions action;
+	private Date date;
+
+	public void SetName(string colmeia_name)
+	{
+		name = colmeia_name;
+	}
+
+	public void SetDate(int day, int month, int year)
+	{
+		date.day = day;
+		date.month = month;
+		date.year = year;
+	}
+
+	//public void SetAction(
+}
+
---
+++ Beehive/Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionManager : MonoBehaviour {
+
+	public DataService ds;
+
+	public GameObject main_menu;
+	public GameObject grelha_colmeias;
+
+	void Awake()
+	{
+		ds = new DataService("test.db");
+		ds.CreateDB();
+	}
+
+	public void PressScannearColmeia()
+	{
+		Debug.Log("SCANNEAR COLMEIA");
+		main_menu.SetActive(false);
+		grelha_colmeias.SetActive(true);
+
+			Colmeia c = ds.InsertColmeia("whatevs", 29,09,87);
+		
+	}
+
+	public void PressVerColmeias()
+	{
+		Debug.Log("VER COLMEIAS");
+	}
+
+}
