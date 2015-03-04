24b119c1ab3f3ceef1f7ce2334ff86e2acdc668c
Nuno Costa <nuno.pinto.costa@gmail.com>
2015-03-04 00:52:27 +0000

    adding meta

---
+++ Beehive/Assets/Scripts/Evento.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using SQLite4Unity3d;
+
+public class Evento {
+	
+	[PrimaryKey, AutoIncrement]
+	public int id { get; set; }
+	public int colmeia_id { get; set; }
+
+	public int day { get; set; }
+	public int month { get; set; }
+	public int year { get; set; }
+
+	public enum TipoEvento {
+		
+		ACCAO_A,
+		ACCAO_B,
+		ACCAO_C,
+		ACCAO_D
+		
+	}
+
+	public TipoEvento tipo_evento;
+
+	public Evento(){}
+
+	public Evento(TipoEvento evento, int colmeia_id, int day, int month, int year)
+	{
+		day = day;
+		month = month;
+		year = year;
+
+		tipo_evento = evento;
+		this.colmeia_id = colmeia_id;
+	}
+}
